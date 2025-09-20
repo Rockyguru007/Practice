8a751af0df75592c9b25c0b6c299cb17750776fc
Rakesh Rosan panda <rakeshrosanpanda101@gmail.com>
2025-09-20 12:38:45 +0530

    Update sum copy 2.py

--- sum copy 2.py
+++ sum copy 2.py
@@ -1,8 +1,8 @@
 # sum.py
 
 # Get input from the user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
 
 # Calculate the sum
 total = num1 + num2
